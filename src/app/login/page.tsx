b2a1b093aff8e356cca41202488a71b6e5dc84bc
ismail855 <ismail135778@gmail.com>
2025-02-01 22:31:23 +0600

    Login page updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
               User Login
             </Typography>
             <Typography variant="body2">
-              Email : user@gmail.com
+              Email : test@gmail.com
             </Typography>
             <Typography variant="body2">
               Password: 1234
